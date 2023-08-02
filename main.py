0683ae1f6af993f40b76ad22414773d1f2e11fc5
Tiago Costa <tiagodpv@gmail.com>
2023-08-02 19:57:50 -0400

    fo

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-> "Filme: Vingadores de 2018 - 160 min"
+url = "https://www.alura.com.br/curso?curso=python"
+indice_curso = url.find("curso")
+indice_valor = indice_curso + len("curso") + 1
+valor = url[indice_valor:]
+print(valor)
